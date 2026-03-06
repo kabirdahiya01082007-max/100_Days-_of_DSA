2c1a37cc6dd7a8a53bd1480e755dde53243e60d0
kabirdahiya01082007-max <kabirdahiya01082007@gmail.com>
2026-03-06 13:27:25 +0530

    Q22

---
+++ Day22.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    struct node * next;
+};
+
+int main() {
+    int n; printf("Enter the no. of nodes >"); scanf("%d", &n);
+    struct node * head = NULL, *x;
+    for (int i = 0; i < n; i++){
+        x = (struct node *)malloc(sizeof(struct node));
+        printf("Enter data of node %d >", i+1); scanf("%d", &x->data);
+        x->next = NULL;
+        if (head == NULL){head = x;}
+        else {
+            struct node * h1 = head;
+            while(h1->next != NULL){h1 = h1->next;}
+            h1->next = x;
+        }
+    }
+    int count = 0;
+    struct node * h1 = head;
+    while(h1 != NULL){
+        count++; h1 = h1->next;
+    }
+    printf("The no. of nodes. is %d", count);
+    return 0;
+}
